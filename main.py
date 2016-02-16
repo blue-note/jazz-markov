89e8a8aa12710f616390bd3bf835897e2ab28b4d
Janita Chalam <janitachalam@gmail.com>
2016-02-16 16:35:14 -0500

    add pickle

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import pykov
 import sys
 import random
 from itertools import cycle
-
-# specify filename on command line
+import shelve
+import os
 
 def train(inputfile):
 
     # inputfile is one-track midi file, 12-bar blues progression in C, 4/4
+
     pattern = midi.read_midifile(inputfile)
     track = pattern[len(pattern)-1] # skip the metadata
     total_num_measures = 12
@@ -19,6 +20,7 @@ def train(inputfile):
     total_ticks = 0
 
     # chains
+
     global_structure = dict()
     global_structure[0] = dict()
     global_structure[1] = dict()
@@ -27,29 +29,31 @@ def train(inputfile):
     global_structure[0]["pitch_chain"] = pykov.Chain()
     global_structure[0]["duration_chain"] = pykov.Chain()
     global_structure[0]["notes_and_rests_chain"] = pykov.Chain()
-    global_structure[0]["num_pitch_tuples"] = 0
-    global_structure[0]["num_duration_tuples"] = 0
 
     global_structure[1]["pitch_chain"] = pykov.Chain()
     global_structure[1]["duration_chain"] = pykov.Chain()
     global_structure[1]["notes_and_rests_chain"] = pykov.Chain()
-    global_structure[1]["num_pitch_tuples"] = 0
-    global_structure[1]["num_duration_tuples"] = 0
 
     global_structure[2]["pitch_chain"] = pykov.Chain()
     global_structure[2]["duration_chain"] = pykov.Chain()
     global_structure[2]["notes_and_rests_chain"] = pykov.Chain()
-    global_structure[2]["num_pitch_tuples"] = 0
-    global_structure[2]["num_duration_tuples"] = 0
 
     # for now, assume even first-order transition probability from note to rest and rest to note
 
-    global_structure[0]["notes_and_rests_chain"][('N','R')] = 0.5
-    global_structure[0]["notes_and_rests_chain"][('R','N')] = 0.5
-    global_structure[1]["notes_and_rests_chain"][('N','R')] = 0.5
-    global_structure[1]["notes_and_rests_chain"][('R','N')] = 0.5
-    global_structure[2]["notes_and_rests_chain"][('N','R')] = 0.5
-    global_structure[2]["notes_and_rests_chain"][('R','N')] = 0.5
+    global_structure[0]["notes_and_rests_chain"][('N','R')] = 0.25
+    global_structure[0]["notes_and_rests_chain"][('N', 'N')] = 0.25
+    global_structure[0]["notes_and_rests_chain"][('R','N')] = 0.25
+    global_structure[0]["notes_and_rests_chain"][('R', 'R')] = 0.25
+
+    global_structure[1]["notes_and_rests_chain"][('N','R')] = 0.25
+    global_structure[1]["notes_and_rests_chain"][('N', 'N')] = 0.25
+    global_structure[1]["notes_and_rests_chain"][('R','N')] = 0.25
+    global_structure[1]["notes_and_rests_chain"][('R', 'R')] = 0.25
+
+    global_structure[2]["notes_and_rests_chain"][('N','R')] = 0.25
+    global_structure[2]["notes_and_rests_chain"][('N', 'N')] = 0.25
+    global_structure[2]["notes_and_rests_chain"][('R','N')] = 0.25
+    global_structure[2]["notes_and_rests_chain"][('R', 'R')] = 0.25
 
     # create cycle on track, iterate through while counting ticks, note the current note and the following as tuples
     # assume there are no simultaneous notes
@@ -65,12 +69,10 @@ def train(inputfile):
         curr_duration = roundtoEighth(fraction_of_measure) # convert note duration to multiple of eigth note (0.125)
         if prev_duration > 0.0 and curr_duration > 0.0:
             global_structure[curr_third]["duration_chain"][(prev_duration, curr_duration)] += 1
-            global_structure[curr_third]["num_duration_tuples"] += 1
         return curr_duration
 
     def save_pitch_transition(prev_note, curr_note):
         global_structure[curr_third]["pitch_chain"][(prev_note, note)] += 1
-        global_structure[curr_third]["num_pitch_tuples"] += 1
 
     while curr_third < 3:
         event = next(iterTrack)
@@ -102,23 +104,58 @@ def train(inputfile):
             curr_third += 1
             ticks_so_far = 0
 
-    #print global_structure
-    #print total_ticks
-    generate(global_structure, 16, 1920)
-
+    d = shelve.open("training_data")
+    try:
+        d["global_structure"] = to_dict(global_structure)
+    finally: 
+        d.close()
 
 def roundtoEighth(x, prec=3, base=0.125):
+
     return round(base * round(float(x)/base), prec)
 
-def generate(global_structure, beats_per_third, ticks_per_measure):
+def to_dict(global_structure):
+    global_structure_new = dict()
+    for key, chain_dict in global_structure.iteritems():
+        all_chains_new = dict() # replaces dict of chain names to chains
+        for name, chain in chain_dict.iteritems():
+            single_chain_new = dict() # replaces pykov chain
+            for t in chain: 
+                single_chain_new[t] = chain[t]
+            all_chains_new[name] = single_chain_new
+        global_structure_new[key] = all_chains_new
+
+    return global_structure_new
+
+def to_pykov_chains(global_structure):
+    global_structure_new = dict()
+    for key, chain_dict in global_structure.iteritems():
+        for name, chain in chain_dict.iteritems():
+            chain_dict[name] = pykov.Chain(chain) # replace dict with pykov chain
+    return global_structure
+
+def generate():
+
+    # get training data from disk
+
+    d = shelve.open("training_data")
+    try:
+        global_structure = to_pykov_chains(d["global_structure"])
+    finally: 
+        d.close()
+
     # generate melody
+
+    resolution = 480
+    ticks_per_measure = resolution * 4
     measures_so_far = 0
     curr_third = 0
     pitch_chain = global_structure[curr_third]["pitch_chain"]
     duration_chain = global_structure[curr_third]["duration_chain"]
     notes_and_rests_chain = global_structure[curr_third]["notes_and_rests_chain"]
 
-    # generated walks
+    # walks to be generated
+
     pitch_walk = list()
     duration_walk = list()
     notes_and_rests_walk = list()
@@ -135,70 +172,31 @@ def generate(global_structure, beats_per_third, ticks_per_measure):
         duration_chain = global_structure[curr_third]["duration_chain"]
         notes_and_rests_chain = global_structure[curr_third]["notes_and_rests_chain"]
 
-    	prev_note = step(prev_note, pitch_chain)
-    	prev_duration = step(prev_duration, duration_chain)
-    	prev_note_type = step(prev_note_type, notes_and_rests_chain)
+        prev_note = step(prev_note, pitch_chain)
+        prev_duration = step(prev_duration, duration_chain)
+        prev_note_type = step(prev_note_type, notes_and_rests_chain)
 
-    	pitch_walk.append(prev_note)
-    	duration_walk.append(prev_duration)
-    	notes_and_rests_walk.append(prev_note_type)
+        pitch_walk.append(prev_note)
+        duration_walk.append(prev_duration)
+        notes_and_rests_walk.append(prev_note_type)
 
-    	measures_so_far += prev_duration
+        measures_so_far += prev_duration
 
-    	if measures_so_far >= 4:
+        if measures_so_far >= 4:
          curr_third += 1
          measures_so_far = 0
 
-
     # generate midi file
 
-    pattern = midi.Pattern(resolution=480)
+    pattern = midi.Pattern(resolution=resolution)
     track = midi.Track()
 
-    # OLD MIDI GEN LOOP
-
-    # get starting state
-
-    # prev_note = pitch_walk[0]
-    # prev_num_ticks = duration_walk[0] * ticks_per_measure
-    # prev_note_type = notes_and_rests_walk[0]
-
-    # for i in xrange(1, len(pitch_walk)):
-    # 	curr_note_type = notes_and_rests_walk[i]
-    # 	curr_duration = duration_walk[i]
-    # 	curr_num_ticks = int(curr_duration * ticks_per_measure)
-    # 	curr_note = pitch_walk[i]
-
-    # 	if prev_note_type == 'N' and curr_note_type == 'N':
-    # 		track.append(midi.NoteOnEvent(tick=curr_num_ticks, data=[curr_note, 80]))
-    # 		track.append(midi.NoteOffEvent(tick=curr_num_ticks, data=[curr_note, 80]))
-    # 		prev_num_ticks = 0
-
-    # 	elif prev_note_type == 'R' and curr_note_type == 'N':
-    # 		#print type(curr_num_ticks)
-    # 		track.append(midi.NoteOnEvent(tick=curr_num_ticks, data=[curr_note, 80]))
-    # 		track.append(midi.NoteOffEvent(tick=curr_num_ticks, data=[curr_note, 80]))
-
-    # 	if prev_note_type == 'R' and curr_note_type == 'R':
-    # 		prev_num_ticks += curr_num_ticks
-
-    # 	else:
-    # 		prev_num_ticks = curr_num_ticks
-
-    # 	prev_note = curr_note
-    # 	prev_note_type = curr_note_type
-
-
-    # NEW MIDI GEN LOOP
-
     prev_num_ticks = 0 # rest tick count
-    total_num_ticks = 0
 
     for i in xrange(len(pitch_walk)):
         curr_note_type = notes_and_rests_walk[i]
         curr_duration = duration_walk[i]
         curr_num_ticks = int(curr_duration * ticks_per_measure) # tick count for this note
-        total_num_ticks += curr_num_ticks
         curr_note = pitch_walk[i]
 
         if curr_note_type == 'N':
@@ -211,62 +209,40 @@ def generate(global_structure, beats_per_third, ticks_per_measure):
 
     track.append(midi.EndOfTrackEvent(tick=1))
     pattern.append(track)
-    print total_num_ticks
-    midi.write_midifile("testing.mid", pattern)
+    midi.write_midifile("testing4.mid", pattern)
 
 def step(state, chain):
-	successors = get_successors(state, chain)
-	if len(successors) == 0:
-		c = random.choice(chain.keys())
-		return c[0]
-	else:
-		return choice(successors)
+    successors = get_successors(state, chain)
+    if len(successors) == 0:
+        c = random.choice(chain.keys())
+        return c[0]
+    else:
+        return choice(successors)
 
 def get_successors(state, chain):
-	# for input state return a dict of successors and their transition probs from the given chain
-	res = pykov.Vector()
-	for s, p in chain.iteritems():
-		if s[0] == state:
-			res[s[1]] = p
-	res.normalize()
-	return res
+    # for input state return a dict of successors and their transition probs from the given chain
+    res = pykov.Vector()
+    for s, p in chain.iteritems():
+        if s[0] == state:
+            res[s[1]] = p
+    res.normalize()
+    return res
 
 def choice(successors):
-	# return random step based on distribution
-	func = random.uniform
-	x = func(0,1)
-	res = random.choice(successors.keys())
-	keys = successors.keys()
-	random.shuffle(keys)
-	for state in keys:
-		prob = successors[state]
-		if x < prob:
-			res = state
-        	break
+    # return random step based on distribution
+    func = random.uniform
+    x = func(0,1)
+    res = random.choice(successors.keys())
+    keys = successors.keys()
+    random.shuffle(keys)
+    for state in keys:
+        prob = successors[state]
+        if x < prob:
+            res = state
+            break
         x = x - prob
-	return res
-
-def test():
-	# Instantiate a MIDI Pattern (contains a list of tracks)
-	pattern = midi.Pattern()
-	# Instantiate a MIDI Track (contains a list of MIDI events)
-	track = midi.Track()
-	# Append the track to the pattern
-	pattern.append(track)
-	# Instantiate a MIDI note on event, append it to the track
-	on = midi.NoteOnEvent(tick=0, velocity=20, pitch=midi.G_3)
-	track.append(on)
-	# Instantiate a MIDI note off event, append it to the track
-	off = midi.NoteOffEvent(tick=100, pitch=midi.G_3)
-	track.append(off)
-	# Add the end of track event, append it to the track
-	eot = midi.EndOfTrackEvent(tick=1)
-	track.append(eot)
-	# Print out the pattern
-	print pattern
-	# Save the pattern to disk
-	midi.write_midifile("example.mid", pattern)
+    return res
 
 if __name__ == "__main__":
-	train(sys.argv[1])
-	#test()
+    train(sys.argv[1])
+    generate()
