4411a78637f4435198af72b1b40549b1a58ddef7
Janita Chalam <janitachalam@gmail.com>
2016-02-18 13:51:35 -0500

    fix untracked files

--- build/scripts-2.7/mididumphw.py
+++
@@ -1,9 +0,0 @@
-#!/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
-"""
-Print a description of the available devices.
-"""
-import midi.sequencer as sequencer
-
-s = sequencer.SequencerHardware()
-
-print s
